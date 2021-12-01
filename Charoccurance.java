7e1f6604f0b9a54102509e4153eeef90248410a8
Ravichandran8208 <95357928+Ravichandran8208@users.noreply.github.com>
2021-12-01 21:13:03 +0530

    Add files via upload

---
+++ Charoccurance.java
@@ -0,0 +1,35 @@
+package week1day2;
+
+public class Charoccurance {
+	
+
+	public static void main(String[] args) {
+		int count=0;
+		
+		
+		String str="welcome chennai";
+		
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)=='e') {
+				count++;
+				
+				
+				
+			}
+		}
+		
+		
+		System.out.println("occurance of time is "+count);
+		
+		
+		
+
+	}
+	
+	
+	
+	
+
+	}
+
+
---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+package week1day2;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String str ="madam";
+		String rev ="";
+		int strlength=str.length();
+		for(int i=(strlength-1);i>=0;i--) {
+			rev=rev+str.charAt(i);
+			
+		}
+		if(str.equals(rev)) {
+			System.out.println(str+ "is a palindrome string");
+		}
+		else {
+			System.out.println(str + "is not a palindrome string");
+		}
+			
+		
+		
+
+	}
+
+}
